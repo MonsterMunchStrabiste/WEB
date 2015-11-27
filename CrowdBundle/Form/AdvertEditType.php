c0e9e5aad67f91ffb75fd55b0c0a5655cd9a6804
AlessandroVilla <alessandro.villa@free.fr>
2015-11-27 16:09:17 +0100

    formulaires

---
+++ CrowdBundle/Form/AdvertEditType.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace WEB\CrowdBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class AdvertEditType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder->remove('date');
+  }
+
+  public function getName()
+  {
+    return 'web_crowdbundle_advert_edit';
+  }
+
+  public function getParent()
+  {
+    return new AdvertType();
+  }
+}
